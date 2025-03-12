d2e55667d7ee2e5561d66d5efcde5bfc11867854
youngjuKim <kjuzoojuk@naver.com>
2025-03-12 16:38:51 +0900

    feat: initial setup

---
+++ src/main/java/com/example/loginBoard/LoginBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBoardApplication.class, args);
+	}
+
+}
